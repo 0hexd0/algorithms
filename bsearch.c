8d12cf553a96819e0a9c15e7e5cf4af971149699
hexd <1514133606@qq.com>
2020-07-20 23:41:59 +0800

    use srand add bsearch_last

--- bsearch.c
+++ bsearch.c
@@ -7,6 +7,8 @@ int bsearch(int arr[], int len, int target);
 
 int bsearch_first(int arr[], int len, int target);
 
+int bsearch_last(int arr[], int len, int target);
+
 int bsearch(int arr[], int len, int target){
   int s = 0; 
   int e = len - 1;
@@ -46,12 +48,33 @@ int bsearch_first(int arr[], int len, int target){
   }
 }
 
+int bsearch_last(int arr[], int len, int target){
+  int s = 0;
+  int e = len - 1;
+  int c;
+  while(s <= e){
+    c = s + ((e - s) >> 2);
+    if(target >= arr[c]){
+      s = c + 1;
+    } else {
+      e = c - 1;
+    }
+  }
+  if(e > -1 && arr[e] == target){
+    return e;
+  } else {
+    return -1;
+  }
+}
+
 void main(){
   int arr[LEN] = {0};
   getArr(arr, LEN);
   quick_sort(arr, 0, LEN - 1);
   printArr(arr, LEN);
   printf("\nfind:%d\n", arr[3]);
-  int result = bsearch_first(arr,LEN,arr[3]);
-  printf("result:%d\n", result);
+  int result1 = bsearch_first(arr,LEN,arr[3]);
+  printf("the first index:%d\n", result1);
+  int result2 = bsearch_last(arr,LEN,arr[3]);
+  printf("the last index:%d\n", result2);
 }
--- util.c
+++ util.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-
+#include<time.h>
 
 void getArr(int arr[], int len){
+  srand(time(NULL));
   for(int i=0;i<len;i++){
-    arr[i] = rand()%100;
+    arr[i] = rand()%10;
   }
 }
 
